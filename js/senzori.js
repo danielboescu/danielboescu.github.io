06bce95a867ac1eefdbb3e8544b2373265225978
danielboescu <dan.boe80@gmail.com>
2017-11-13 19:12:50 +0200

    Am citit sezor orientare

--- js/senzori.js
+++ js/senzori.js
@@ -1,1 +1,8 @@
-document.getElementById("id_x").innerHTML = "Salut";
+//document.getElementById("id_x").innerHTML = "Salut";
+function on_device_orientation(e)
+{
+	document.getElementById("id_z").innerHTML = e.alpha;
+	document.getElementById("id_y").innerHTML = e.gama;
+	document.getElementById("id_x").innerHTML = e.beta;
+}
+window.addEventListener("deviceorientation",on_device_orientation);
