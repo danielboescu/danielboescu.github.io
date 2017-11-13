c034e3d4c1f69a3a04ae67e424bfeee0d4632b7e
danielboescu <dan.boe80@gmail.com>
2017-11-13 19:31:47 +0200

    spatiu

--- js/senzori.js
+++ js/senzori.js
@@ -1,8 +1,8 @@
 //document.getElementById("id_x").innerHTML = "Salut";hghghgg
 function on_device_orientation(e)
 {
-	document.getElementById("id_z").innerHTML ="z" + e.alpha;
-	document.getElementById("id_y").innerHTML ="y" + e.gamma;
-	document.getElementById("id_x").innerHTML ="x" + e.beta;
+	document.getElementById("id_z").innerHTML ="z " + e.alpha;
+	document.getElementById("id_y").innerHTML ="y " + e.gamma;
+	document.getElementById("id_x").innerHTML ="x " + e.beta;
 }
 window.addEventListener("deviceorientation",on_device_orientation);
