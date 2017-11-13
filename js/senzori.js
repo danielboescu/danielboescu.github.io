5493a9d1f9261516fcfa54db17ca0bb4faa3b39a
danielboescu <dan.boe80@gmail.com>
2017-11-13 19:14:28 +0200

    gamma

--- js/senzori.js
+++ js/senzori.js
@@ -2,7 +2,7 @@
 function on_device_orientation(e)
 {
 	document.getElementById("id_z").innerHTML = e.alpha;
-	document.getElementById("id_y").innerHTML = e.gama;
+	document.getElementById("id_y").innerHTML = e.gamma;
 	document.getElementById("id_x").innerHTML = e.beta;
 }
 window.addEventListener("deviceorientation",on_device_orientation);
