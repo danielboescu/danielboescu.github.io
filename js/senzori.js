8cecb7bfe608894901d864266f31949dd9f9ab3f
danielboescu <dan.boe80@gmail.com>
2017-11-13 19:40:19 +0200

    am inceput device motion

--- js/senzori.js
+++ js/senzori.js
@@ -1,8 +1,14 @@
 //document.getElementById("id_x").innerHTML = "Salut";hghghgg
+window.addEventListener("deviceorientation",on_device_orientation);
+window.addEventListener("devicemotion",on_device_motion);
 function on_device_orientation(e)
 {
 	document.getElementById("id_z").innerHTML ="z " + e.alpha;
 	document.getElementById("id_y").innerHTML ="y " + e.gamma;
 	document.getElementById("id_x").innerHTML ="x " + e.beta;
 }
-window.addEventListener("deviceorientation",on_device_orientation);
+
+function on_device_motion(e)
+{
+	
+}
