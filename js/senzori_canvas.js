7ea867ee21cf986693814c48a304fba8ba1bdda7
danielboescu <dan.boe80@gmail.com>
2017-11-27 19:10:39 +0200

    a

--- js/senzori_canvas.js
+++ js/senzori_canvas.js
@@ -1,4 +1,4 @@
- document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.20.3";
+ document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.20.4";
  window.addEventListener("deviceorientation",on_device_orientation);
 
  //---------------------------------------------------------------
@@ -7,8 +7,9 @@
 	var canvas = document.getElementById("id_canvas");
 	var ctx = canvas.getContext("2d");
 	ctx.clearRect(0,0,400,400);
-	ctx.stroke();
+
     ctx.rect(0,0,400,400);
+		ctx.stroke();
 	ctx.beginPath();
 	ctx.arc(200 + unghi1*200/90,200 + unghi2*200/90,20,0,2*Math.PI,true);
 	ctx.fillStyle="#FF0000";//se umple cercul cu culoarea este rosu
