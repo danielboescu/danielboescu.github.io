0d8df7e9b7b5b762b453d478b71baef1d5f5d621
danielboescu <dan.boe80@gmail.com>
2017-11-27 18:13:56 +0200

    arc de cerc

--- js/canvas.js
+++ js/canvas.js
@@ -1,11 +1,8 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.27.1";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.27.2";
 
 var canvas = document.getElementById("id_canvas");
 var ctx = canvas.getContext("2d");
 
 ctx.beginPath();
-ctx.moveTo(0, 0);
-ctx.lineTo(100, 100);
-ctx.lineTo(120, 50);
-ctx.closePath();
+ctx.arc(100,100,20,0,Math.PI/2,true);
 ctx.stroke();
