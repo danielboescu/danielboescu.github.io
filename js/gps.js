a2082c46ca8fd9d50df4f592ca741fa974a74867
danielboescu <dan.boe80@gmail.com>
2017-12-14 19:22:13 +0200

    mod2

--- js/gps.js
+++ js/gps.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.0";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.1";
 
 navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
 
