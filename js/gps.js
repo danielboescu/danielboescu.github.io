58e60a52bc1bf1a6c057c425df015245c5480cb2
danielboescu <dan.boe80@gmail.com>
2017-12-14 19:30:41 +0200

    altitude

--- js/gps.js
+++ js/gps.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.1";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.2";
 
 navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
 
@@ -8,6 +8,7 @@ function on_position_success(e)
 	 document.getElementById("id_lat").innerHTML = "Latitude =" +e.coords.latitude;
 	 document.getElementById("id_long").innerHTML ="Longitude =" +e.coords.longitude;
 	 document.getElementById("id_acc").innerHTML ="Accuracy =" +e.coords.accuracy + "m";
+	  document.getElementById("id_alt").innerHTML ="Altitude =" +e.coords.altitude + "m";
 	
 }
 //#########################################
