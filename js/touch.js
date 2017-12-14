bd7e447b5d25ceea0b8049d43f3167d7232410a2
danielboescu <dan.boe80@gmail.com>
2017-12-14 18:21:48 +0200

    modificare

--- js/touch.js
+++ js/touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.04.2";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.0";
 
 
 var canvas = document.getElementById("id_canvas");
@@ -8,11 +8,11 @@ canvas.addEventListener("touchstart",on_start_touch);
 //---------------------------------------
 function on_start_touch(e)
 {
-var touches = e.changedTouches;
-for(var i=0; i<touches.length; i++)
+var atingere = e.touches;
+for(var i=0; i<atingere.length; i++)
 {
 	context.beginPath();
-	context.arc(touches.item(i).pageX,touches.item(i).pageY,20,0,2*Math.PI);
+	context.arc(atingere.item(i).pageX,atingere.item(i).pageY,20,0,2*Math.PI);
 	context.strokeStyle=lista_culori[i];
 	context.lineWidth=5;
 	context.stroke(); 
