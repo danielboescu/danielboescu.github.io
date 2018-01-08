22d674fac7b6b8980174ed5da7727f923ca3075e
danielboescu <dan.boe80@gmail.com>
2018-01-08 18:44:15 +0200

    a

--- js/gps.js
+++ js/gps.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2018.01.08.2";
+document.getElementById("id_business_version").innerHTML = "Business version = 2018.01.08.3";
 
 navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
 
@@ -12,7 +12,7 @@ function on_position_success(e)
 	  var map_str="https://maps.googleapis.com/maps/api/staticmap?center="+
 	  e.coords.latitude + "," + e.coords.longitude + "&zoom=18"+"&size=400x300"+
 "&key=AIzaSyDvoY0i_x0wXeE7vAOztYvmCzDIfEtzAR0"+
-"&markers=color:blue|label:APOR|"+
+"&markers=color:blue|label:A|"+
 	  e.coords.latitude + "," + e.coords.longitude;
 document.getElementById("id_img").src=map_str;
 	
