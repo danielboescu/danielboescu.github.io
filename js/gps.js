005f13592488ce0e8ae879b40d4d88459d0721c5
danielboescu <dan.boe80@gmail.com>
2018-01-08 17:46:22 +0200

    imagine

--- js/gps.js
+++ js/gps.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.2";
+document.getElementById("id_business_version").innerHTML = "Business version = 2018.01.08.0";
 
 navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
 
@@ -9,6 +9,10 @@ function on_position_success(e)
 	 document.getElementById("id_long").innerHTML ="Longitude =" +e.coords.longitude;
 	 document.getElementById("id_acc").innerHTML ="Accuracy =" +e.coords.accuracy + "m";
 	  document.getElementById("id_alt").innerHTML ="Altitude =" +e.coords.altitude + "m";
+	  var map_str="https://maps.googleapis.com/maps/api/staticmap?center"+
+	  e.coords.latitude + ","+ e.coords.longitude +"&zoom=13"+"&size=400x300"+
+"&key=AIzaSyBbDa93UfZqCpczIfiM1h_VFTnxWIFKdGI"
+document.getElementById("id_img").src=map_str;
 	
 }
 //#########################################
@@ -18,3 +22,5 @@ function on_position_failure(e)
 	alert("I'm lost!");
 	
 }
+cheia api google
+AIzaSyBbDa93UfZqCpczIfiM1h_VFTnxWIFKdGI
