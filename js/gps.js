f00e33b45eb3e606c7292ce08eb811cc0718f97c
danielboescu <dan.boe80@gmail.com>
2018-01-08 17:52:07 +0200

    d
    ;

--- js/gps.js
+++ js/gps.js
@@ -10,8 +10,8 @@ function on_position_success(e)
 	 document.getElementById("id_acc").innerHTML ="Accuracy =" +e.coords.accuracy + "m";
 	  document.getElementById("id_alt").innerHTML ="Altitude =" +e.coords.altitude + "m";
 	  var map_str="https://maps.googleapis.com/maps/api/staticmap?center"+
-	  e.coords.latitude + ","+ e.coords.longitude +"&zoom=15"+"&size=400x300"+
-"&key=AIzaSyBbDa93UfZqCpczIfiM1h_VFTnxWIFKdGI"
+	  e.coords.latitude + "," + e.coords.longitude + "&zoom=15"+"&size=400x300"+
+"&key=AIzaSyBbDa93UfZqCpczIfiM1h_VFTnxWIFKdGI";
 document.getElementById("id_img").src=map_str;
 	
 }
