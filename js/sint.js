ac625d855e1bf7d18062bb751bbc6047e841e3f5
danielboescu <dan.boe80@gmail.com>
2018-01-15 18:05:54 +0200

    dezacticare touch start

--- js/sint.js
+++ js/sint.js
@@ -1,7 +1,7 @@
 var d = new Date();
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".1"; 
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".2"; 
 
-document.addEventListener("touchstart",speak);
+//document.addEventListener("touchstart",speak);
 
 var synth= window.speechSynthesis;
 
