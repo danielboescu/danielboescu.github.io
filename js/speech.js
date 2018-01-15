5e0bc2f27985dee85aed3aa0e9c8ccfb68ed7d7f
danielboescu <dan.boe80@gmail.com>
2018-01-15 19:24:46 +0200

    confidence

--- js/speech.js
+++ js/speech.js
@@ -1,5 +1,5 @@
 var d = new Date();
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".4"; 
 
 var speech = new webkitSpeechRecognition();
 speech.onresult = on_speech_results;
@@ -21,6 +21,6 @@ function on_speech_end()
 //####################################################
 function on_speech_results(e)
 {
-	document.getElementById("id_speech").innerHTML=e.results.item[0][0].transcript;
+	document.getElementById("id_speech").innerHTML=e.results.item[0][0].transcript + " "  + e.results.item[0][0].confidence ;
 	
 }
