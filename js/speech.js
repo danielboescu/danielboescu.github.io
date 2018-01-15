97e4dc222befad0451a402fa6a5ce748ec425794
danielboescu <dan.boe80@gmail.com>
2018-01-15 19:01:39 +0200

    speech recog

---
+++ js/speech.js
@@ -0,0 +1,26 @@
+var d = new Date();
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0"; 
+
+var speech = new SpeechRecognition();
+speech.onresult = on_speech_results;
+speech.onspeechend = on_speech_end;
+speech.lang="en-US";
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+function recognize()
+{
+	speech.start();
+}
+
+//####################################################
+
+function on_speech_end()
+{
+		speech.stop();
+	
+}
+//####################################################
+function on_speech_results(e)
+{
+	document.getElementById("id_speech")=e.results.item[0].item[0].transcript;
+	
+}
