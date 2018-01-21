e6d11e7d21913ecae9bd98e04cf174e929b6f642
danielboescu <dan.boe80@gmail.com>
2018-01-21 22:00:07 +0200

    tema ultim

---
+++ js/speech_direction.js
@@ -0,0 +1,54 @@
+document.getElementById("id_business_version").innerHTML = "Business version = 2018.01.08.4";
+
+navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+var speech = new webkitSpeechRecognition();
+speech.maxAlternatives = 5;
+speech.onresult = on_speech_results;
+speech.onspeechend = on_speech_end;
+speech.lang="en-US";
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+function recognize()
+{
+	speech.start();
+}
+
+//####################################################
+
+function on_speech_end()
+{
+		speech.stop();
+	
+}
+//####################################################
+function on_speech_results(e)
+{
+	for(var i= 0; i<speech.maxAlternatives; i++)
+	{
+	document.getElementById("id_speech").innerHTML+=e.results[0][i].transcript;// + " "  + e.results[0][i].confidence +"<br>";
+	}
+}
+//#########################################
+function on_position_success(e)
+{
+	 document.getElementById("id_lat").innerHTML = "Latitude =" +e.coords.latitude;
+	 document.getElementById("id_long").innerHTML ="Longitude =" +e.coords.longitude;
+	 document.getElementById("id_acc").innerHTML ="Accuracy =" +e.coords.accuracy + "m";
+	 document.getElementById("id_alt").innerHTML ="Altitude =" +e.coords.altitude + "m";
+	  var map_str="https://maps.googleapis.com/maps/api/staticmap?center="+
+	  e.coords.latitude + "," + e.coords.longitude + "&zoom=15"+"&size=400x300"+
+"&key=AIzaSyDvoY0i_x0wXeE7vAOztYvmCzDIfEtzAR0"+
+"&markers=color:blue|label:A|"+
+	  e.coords.latitude  + "," + e.coords.longitude + 
+	  "&path=color:red|" + e.coords.latitude + "," + e.coords.longitude + "|" + document.getElementById("id_speech").innerHTML
+	  ;
+document.getElementById("id_img").src=map_str;
+	
+}
+//#########################################
+function on_position_failure(e)
+{
+	
+	alert("I'm lost!");
+	
+}
