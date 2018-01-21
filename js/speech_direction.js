c51ea74984390a4bb18628ef17124a45b6b22c87
danielboescu <dan.boe80@gmail.com>
2018-01-21 22:49:00 +0200

    let me see

--- js/speech_direction.js
+++ js/speech_direction.js
@@ -1,7 +1,7 @@
 var d = new Date();
 document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".1"; 
 
-navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
+
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 var speech = new webkitSpeechRecognition();
 //speech.maxAlternatives = 5;
@@ -27,9 +27,11 @@ function on_speech_results(e)
 	//for(var i= 0; i<speech.maxAlternatives; i++)
 //	{
 	document.getElementById("id_speech").innerHTML+=e.results[0][0].transcript;// + " "  + e.results[0][i].confidence +"<br>";
+	navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
 	//}
 }
 //#########################################
+
 function on_position_success(e)
 {
 	 document.getElementById("id_lat").innerHTML = "Latitude =" +e.coords.latitude;
@@ -39,7 +41,7 @@ function on_position_success(e)
 	  var map_str="https://maps.googleapis.com/maps/api/staticmap?center="+
 	  e.coords.latitude + "," + e.coords.longitude + "&zoom=15"+"&size=400x300"+
 "&key=AIzaSyDvoY0i_x0wXeE7vAOztYvmCzDIfEtzAR0"+
-"&markers=color:blue|label:A|"+
+"&markers=color:blue|label:Z|"+
 	  e.coords.latitude  + "," + e.coords.longitude + 
 	  "&path=color:red|" + e.coords.latitude + "," + e.coords.longitude + "|" + document.getElementById("id_speech").innerHTML
 	  ;
