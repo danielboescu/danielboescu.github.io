5c7427a5e27ee9ac3453bb007c734fa118447cac
danielboescu <dan.boe80@gmail.com>
2018-01-22 18:12:33 +0200

    cam spate

--- js/cam.js
+++ js/cam.js
@@ -1,11 +1,11 @@
 var d = new Date();
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0"; 
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".1"; 
 //_______________________________________________###____________________________________________
-var constraints={audio: true,video: true};
+var constraints={audio: true,video: { facingMode: { exact: "environment" } }};
 navigator.mediaDevices.getUserMedia(constraints).then(on_cam).catch(on_error);
 var video = document.getElementById("id_video");
 
-//^^^^^^^^^^^^^^^^6
+//^^^^^^^^^^^^^^^^
 function on_cam(stream)
 {
 	video.srcObject=stream;
