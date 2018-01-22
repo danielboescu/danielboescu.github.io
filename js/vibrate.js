0ca029972b95596d528ece8ead2e74b48c051164
danielboescu <dan.boe80@gmail.com>
2018-01-22 18:59:06 +0200

    morse

--- js/vibrate.js
+++ js/vibrate.js
@@ -1,9 +1,9 @@
 var d = new Date();
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".1"; 
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".2"; 
 //_______________________________________________###____________________________________________
 var image = document.getElementById("id_img");
 image.addEventListener("touchstart",on_touch_img);
 function on_touch_img(e)
 {
-window.navigator.vibrate(200);
+window.navigator.vibrate([100,30,100,30,100,200,200,30,200,30,200,200,100,30,100,30,100]);
 }
