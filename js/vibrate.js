a65f62550afeb7afb0794946f2dbc75f8728202a
danielboescu <dan.boe80@gmail.com>
2018-01-22 19:11:16 +0200

    atingere

--- js/vibrate.js
+++ js/vibrate.js
@@ -2,8 +2,16 @@ var d = new Date();
 document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".2"; 
 //_______________________________________________###____________________________________________
 var image = document.getElementById("id_img");
-image.addEventListener("touchstart",on_touch_img);
-function on_touch_img(e)
+image.addEventListener("touchstart",on_touch_start);
+image.addEventListener("touchend",on_touch_end);
+
+function on_touch_start(e)
 {
-window.navigator.vibrate([100,30,100,30,100,200,200,30,200,30,200,200,100,30,100,30,100]);
+setInterval(start_vibrate,100);
+
+}
+function start_vibrate()
+{
+	
+	window.navigator.vibrate(100);
 }
