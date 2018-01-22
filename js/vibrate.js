6bdb32f5aa05b9289a7d725e096a7ef597a69ceb
danielboescu <dan.boe80@gmail.com>
2018-01-22 19:14:02 +0200

    test

--- js/vibrate.js
+++ js/vibrate.js
@@ -1,5 +1,5 @@
 var d = new Date();
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".2"; 
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
 //_______________________________________________###____________________________________________
 var image = document.getElementById("id_img");
 image.addEventListener("touchstart",on_touch_start);
@@ -7,11 +7,15 @@ image.addEventListener("touchend",on_touch_end);
 
 function on_touch_start(e)
 {
-setInterval(start_vibrate,100);
+timer_id = setInterval(start_vibrate,100);
 
 }
 function start_vibrate()
 {
 	
-	window.navigator.vibrate(100);
+	window.navigator.vibrate(150);
+}
+function on_touch_end()
+{
+	clearInterval(timer_id);
 }
