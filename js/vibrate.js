395bb9bbbefad73cf546c390e96e2e8f63ac9b36
danielboescu <dan.boe80@gmail.com>
2018-01-22 19:21:05 +0200

    default

--- js/vibrate.js
+++ js/vibrate.js
@@ -1,5 +1,5 @@
 var d = new Date();
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".4"; 
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".5"; 
 //_______________________________________________###____________________________________________
 var image = document.getElementById("id_img");
 image.addEventListener("touchstart",on_touch_start);
@@ -7,6 +7,7 @@ image.addEventListener("touchend",on_touch_end);
 
 function on_touch_start(e)
 {
+	e.preventDefault();
 timer_id = setInterval(start_vibrate,100);
 
 }
