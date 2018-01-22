6b36b3f11cac82262e7f66c416a47d4d394217d7
danielboescu <dan.boe80@gmail.com>
2018-01-22 18:31:12 +0200

    draw

--- js/cam.js
+++ js/cam.js
@@ -4,6 +4,7 @@ document.getElementById("id_business_version").innerHTML = "Business version = "
 var constraints={audio: true,video: { facingMode: { exact: "environment" } }};
 navigator.mediaDevices.getUserMedia(constraints).then(on_cam).catch(on_error);
 var video = document.getElementById("id_video");
+video.addEventListener("touchstart",on_touch_video);
 
 //^^^^^^^^^^^^^^^^
 function on_cam(stream)
@@ -16,3 +17,10 @@ function on_error(e)
 	alert("ERROR!: cannot connect to camera!");
 }
 //^^^^^^^^^^^^^^^^6
+function on_touch_video(e)
+{
+	var canvas = document.getElementById("id_img");
+	var context = canvas.getContext("2d");
+	ctx.drawImage(video,0,0);
+
+}
