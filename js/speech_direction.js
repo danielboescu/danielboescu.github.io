b802e04d0e09a30240b251bf88078e1f6bb1d49d
danielboescu <dan.boe80@gmail.com>
2018-01-24 13:23:53 +0200

    sper final

--- js/speech_direction.js
+++ js/speech_direction.js
@@ -1,5 +1,5 @@
 var d = new Date();
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".1"; 
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".2"; 
 navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
 
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -24,11 +24,16 @@ function on_speech_end()
 //####################################################
 function on_speech_results(e)
 {
-	//for(var i= 0; i<speech.maxAlternatives; i++)
-//	{
-	document.getElementById("id_speech").innerHTML=e.results[0][0].transcript;// + " "  + e.results[0][i].confidence +"<br>";
+	document.getElementById("id_speech").innerHTML=e.results[0][0].transcript;
+	var map_str="https://maps.googleapis.com/maps/api/staticmap?center="+
+	  e.coords.latitude + "," + e.coords.longitude + "&zoom=15"+"&size=400x300"+
+"&key=AIzaSyDvoY0i_x0wXeE7vAOztYvmCzDIfEtzAR0"+
+"&markers=color:blue|label:Z|"+
+	  e.coords.latitude  + "," + e.coords.longitude + 
+	  "&path=color:red|" + e.coords.latitude + "," + e.coords.longitude + "|" + document.getElementById("id_speech").innerHTML
+	  ;
+document.getElementById("id_img").src=map_str;
 	
-	//}
 }
 //#########################################
 
@@ -38,8 +43,7 @@ function on_position_success(e)
 	  e.coords.latitude + "," + e.coords.longitude + "&zoom=15"+"&size=400x300"+
 "&key=AIzaSyDvoY0i_x0wXeE7vAOztYvmCzDIfEtzAR0"+
 "&markers=color:blue|label:Z|"+
-	  e.coords.latitude  + "," + e.coords.longitude //+ 
-	 // "&path=color:red|" + e.coords.latitude + "," + e.coords.longitude + "|" + document.getElementById("id_speech").innerHTML
+	  e.coords.latitude  + "," + e.coords.longitude 
 	  ;
 document.getElementById("id_img").src=map_str;
 
