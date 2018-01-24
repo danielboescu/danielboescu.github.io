6204522145d528212b530e642e967b49e1f4dc29
danielboescu <dan.boe80@gmail.com>
2018-01-24 14:40:23 +0200

    final

--- js/speech_direction.js
+++ js/speech_direction.js
@@ -3,10 +3,10 @@ document.getElementById("id_business_version").innerHTML = "Business version = "
 navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
 
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
 var speech = new webkitSpeechRecognition();
 var lat ;
 var lon ; 
-
 speech.onresult = on_speech_results;
 speech.onspeechend = on_speech_end;
 speech.lang="en-US";
@@ -15,7 +15,7 @@ speech.lang="en-US";
 function recognize()
 {
 	if(speech.start())
-	{}
+	{}//rezolvare eroare incercare multipla de pornire speech recognition
 }
 
 //####################################################
@@ -29,35 +29,35 @@ function on_speech_end()
 function on_speech_results(e)
 {
 	document.getElementById("id_speech").innerHTML=e.results[0][0].transcript;
-	line();
+	line(); // functia care traseaza linia
 }
 //#########################################
 
-function on_position_success(e)
+function on_position_success(e) // functia care prezinta locatia utilizatorului
 { 
-lat = e.coords.latitude;
-lon = e.coords.longitude;
+	lat = e.coords.latitude;
+	lon = e.coords.longitude;
 
 	  var map_str="https://maps.googleapis.com/maps/api/staticmap?center="+
-	  e.coords.latitude + "," + e.coords.longitude + "&zoom=15"+"&size=400x300"+
+	  e.coords.latitude + "," + e.coords.longitude + "&zoom=15"+"&size=600x500"+
 "&key=AIzaSyDvoY0i_x0wXeE7vAOztYvmCzDIfEtzAR0"+
 "&markers=color:blue|label:Z|"+
 	  e.coords.latitude  + "," + e.coords.longitude 
 	  ;
-document.getElementById("id_img").src=map_str;
+	document.getElementById("id_img").src=map_str;
 	
 }
 //#########################################
 function line(e)
 {
 		var map_str="https://maps.googleapis.com/maps/api/staticmap?center="+
-	  lat + "," + lon + "&zoom=15"+"&size=400x300"+
+	  lat + "," + lon + "&zoom=15"+"&size=600x500"+
 "&key=AIzaSyDvoY0i_x0wXeE7vAOztYvmCzDIfEtzAR0"+
 "&markers=color:blue|label:Z|"+
 	  lat  + "," + lon + 
 	  "&path=color:red|" + lat + "," + lon + "|" + document.getElementById("id_speech").innerHTML
 	  ;
-document.getElementById("id_img").src=map_str;
+	document.getElementById("id_img").src=map_str;
 		
 	
 }
